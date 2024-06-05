dc81cea840ce316f580bd811f7ab1bf19d963d8e
Stavya3 <Stavyakc03@gmail.com>
2024-06-05 15:09:22 +0530

    Created another grid more efficiently using display:grid, and made the size dynamic

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const gridContainer = document.querySelector("#container");
-for (let i = 0; i < 16 * 16; i++) {
-    let gridItem = document.createElement("div");
-    gridItem.classList.add('gridItem');
-    gridContainer.appendChild(gridItem);
+// const gridContainer = document.querySelector("#container");
+// for (let i = 0; i < 16 * 16; i++) {
+//     let gridItem = document.createElement("div");
+//     gridItem.classList.add('gridItem');
+//     gridContainer.appendChild(gridItem);
     
+// }
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(16);
+});
+
+function createBoard(size){
+    let board = document.querySelector(".container")
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor = "yellow"
+        board.insertAdjacentElement("beforeend", div);
+    }
 }

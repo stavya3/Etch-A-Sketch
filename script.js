c0d48a9f847807ef50937527cf047a2410a1905a
Stavya3 <Stavyakc03@gmail.com>
2024-06-06 16:12:44 +0530

    Added the functionality to control when to draw and when to stop by just clicking once anywhere

--- script.js
+++ script.js
@@ -7,8 +7,24 @@
 // }
 
 let color = "black";
+let click = false;
+
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
+
+
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if (click){
+                draw.innerHTML = "Now You can Draw!";
+            } 
+            else {
+                draw.innerHTML = "Now You cannot Draw :(";
+            }
+        }
+    });
 
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click", function(){
@@ -50,12 +66,14 @@ function getSize(){
 
 
 function colorDiv(){
-    if (color == 'random'){
-        this.style.backgroundColor = `hsl(${Math.random() *360}, 100%, 50%)`;
-    }
-    else {
-        this.style.backgroundColor = "black";
-
+    if (click){
+        if (color == 'random'){
+            this.style.backgroundColor = `hsl(${Math.random() *360}, 100%, 50%)`;
+        }
+        else {
+            this.style.backgroundColor = "black";
+    
+        }
     }
 }
 function setColor(colorChoice){
